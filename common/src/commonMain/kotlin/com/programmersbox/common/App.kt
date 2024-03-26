343aaf175c695a6da21cb33c465f6b3fa07d12f1
jacobrein <jacob.rein@gabb.com>
2024-03-26 14:05:46 -0600

    Implement new game and draw features in Solitaire
    The main functions added in this commit include a new game and draw feature in the Solitaire code. The new game function empties the deck and creates a new, shuffled deck. The draw function has been modified to allow reusing of the discarded cards once the deck is depleted. There's also a small refactoring of the UI code for better layout and coherency.

--- common/src/commonMain/kotlin/com/programmersbox/common/App.kt
+++ common/src/commonMain/kotlin/com/programmersbox/common/App.kt
@@ -1,15 +1,10 @@
 package com.programmersbox.common
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.Surface
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
@@ -17,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.times
 import moe.tlaster.precompose.PreComposeApp
 import moe.tlaster.precompose.viewmodel.ViewModel
 import moe.tlaster.precompose.viewmodel.viewModel
@@ -38,11 +32,12 @@ internal fun App() {
 
 class CardLocation(val location: Int, val card: Card, val place: Int)
 
+//TODO: Make sure to change these to 3 and 13
 const val DRAW_AMOUNT = 1
 const val WIN_CARD_VALUE = 1
 
 class SolitaireInfo(
-    val deck: Deck<Card>,
+    val deck: Deck<Card> = Deck.defaultDeck(),
 ) : ViewModel() {
     var cardLoc: CardLocation? by mutableStateOf(null)
 
@@ -72,6 +67,23 @@ class SolitaireInfo(
 
             }
         }
+        newGame()
+    }
+
+    fun draw() {
+        repeat(DRAW_AMOUNT) {
+            if (deck.size > 0) {
+                drawList.add(deck.draw())
+            } else {
+                deck.addCard(*drawList.toTypedArray())
+                drawList.clear()
+            }
+        }
+    }
+
+    fun newGame() {
+        deck.removeAllCards()
+        deck.addDeck(Deck.defaultDeck())
         deck.shuffle()
         repeat(7) {
             fieldSlots[it] = FieldSlot(it, deck)
@@ -79,10 +91,10 @@ class SolitaireInfo(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Solitaire(
-    deck: Deck<Card> = rememberDeck(),
-    info: SolitaireInfo = viewModel(SolitaireInfo::class) { SolitaireInfo(deck) },
+    info: SolitaireInfo = viewModel(SolitaireInfo::class) { SolitaireInfo() },
 ) {
     val cardSizeModifier = Modifier.height(150.dp)
 
@@ -93,149 +105,148 @@ private fun Solitaire(
     DragDropBox {
         Scaffold(
             topBar = {
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(4.dp),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .systemBarsPadding()
+                TopAppBar(
+                    title = {},
+                    actions = {
+                        //TODO: Add dialog for confirmation
+                        TextButton(
+                            onClick = { info.newGame() }
+                        ) { Text("New Game") }
+                    }
+                )
+            }
+        ) { padding ->
+            Column(
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(padding)
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    info.foundations.forEach { foundation ->
+                        DropTarget<CardLocation>(
+                            onDrop = {
+                                it?.let { cardLocation ->
+                                    if (foundationCheck(cardLocation.card, foundation.value)) {
+                                        foundation.value.add(cardLocation.card)
+                                        when (cardLocation.location) {
+                                            -1 -> info.drawList.remove(cardLocation.card)
+                                            else -> {
+                                                info.fieldSlots[cardLocation.location]?.let { f ->
+                                                    f.removeCard()
+                                                    f.flipFaceDownCard()
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                            modifier = Modifier.weight(1f)
+                        ) { d, f ->
+                            foundation.value.lastOrNull()
+                                ?.let {
+                                    PlayingCard(
+                                        card = it,
+                                        modifier = cardSizeModifier
+                                    )
+                                }
+                                ?: EmptyCard(
+                                    modifier = cardSizeModifier.fillMaxSize(),
+                                    border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
+                                )
+                        }
+                    }
+                }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.End),
+                    modifier = Modifier.fillMaxWidth()
                 ) {
                     Row(
-                        horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
-                        modifier = Modifier.fillMaxWidth()
+                        horizontalArrangement = Arrangement.spacedBy((-50).dp)
                     ) {
-                        info.foundations.forEach { foundation ->
+                        info
+                            .drawList
+                            .dropLast(1)
+                            .takeLast(2)
+                            .forEach {
+                                PlayingCard(
+                                    card = it,
+                                    modifier = cardSizeModifier.width(100.dp)
+                                )
+                            }
+
+                        info.drawList.lastOrNull()?.let {
+                            DragTarget(
+                                dataToDrop = CardLocation(-1, it, 0)
+                            ) {
+                                PlayingCard(
+                                    card = it,
+                                    modifier = cardSizeModifier.width(100.dp)
+                                )
+                            }
+                        } ?: EmptyCard(modifier = cardSizeModifier.width(100.dp))
+                    }
+
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
+                        EmptyCard(
+                            modifier = cardSizeModifier.width(100.dp),
+                        ) { info.draw() }
+                        Text("Cards left: ${info.cardsLeft}")
+                    }
+                }
+                //---------field
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    info.fieldSlots.forEach { fieldSlot ->
+                        Column(
+                            modifier = Modifier.weight(1f)
+                        ) {
                             DropTarget<CardLocation>(
                                 onDrop = {
                                     it?.let { cardLocation ->
-                                        if (foundationCheck(cardLocation.card, foundation.value)) {
-                                            foundation.value.add(cardLocation.card)
+                                        if (fieldCheck(cardLocation.card, fieldSlot.value)) {
                                             when (cardLocation.location) {
-                                                -1 -> info.drawList.remove(cardLocation.card)
+                                                -1 -> {
+                                                    fieldSlot.value.addCard(cardLocation.card)
+                                                    info.drawList.remove(cardLocation.card)
+                                                }
+
                                                 else -> {
-                                                    info.fieldSlots[cardLocation.location]?.let { f ->
-                                                        f.removeCard()
-                                                        f.flipFaceDownCard()
+                                                    info.fieldSlots[cardLocation.location]?.let { f: FieldSlot ->
+                                                        fieldSlot.value.addCards(f.removeCards(cardLocation.place))
                                                     }
                                                 }
                                             }
                                         }
                                     }
-                                },
-                                modifier = Modifier.weight(1f)
+                                }
                             ) { d, f ->
-                                foundation.value.lastOrNull()
-                                    ?.let {
-                                        PlayingCard(
-                                            card = it,
-                                            modifier = cardSizeModifier
-                                        )
-                                    }
-                                    ?: EmptyCard(
+                                if (fieldSlot.value.list.isEmpty()) {
+                                    EmptyCard(
                                         modifier = cardSizeModifier.fillMaxSize(),
                                         border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
                                     )
-                            }
-                        }
-                    }
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.End),
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy((-50).dp)
-                        ) {
-                            info
-                                .drawList
-                                .dropLast(1)
-                                .takeLast(2)
-                                .forEach {
-                                    PlayingCard(
-                                        card = it,
-                                        modifier = cardSizeModifier.width(100.dp)
-                                    )
-                                }
-
-                            info.drawList.lastOrNull()?.let {
-                                DragTarget(
-                                    dataToDrop = CardLocation(-1, it, 0)
-                                ) {
-                                    PlayingCard(
-                                        card = it,
-                                        modifier = cardSizeModifier.width(100.dp)
-                                    )
-                                }
-                            } ?: EmptyCard(modifier = cardSizeModifier.width(100.dp))
-                        }
-
-                        Box(
-                            contentAlignment = Alignment.Center
-                        ) {
-                            EmptyCard(
-                                modifier = cardSizeModifier.width(100.dp),
-                            ) {
-                                repeat(DRAW_AMOUNT) {
-                                    if (deck.size > 0) {
-                                        info.drawList.add(deck.draw())
-                                    } else {
-                                        deck.addCard(*info.drawList.toTypedArray())
-                                        info.drawList.clear()
-                                    }
-                                }
-                            }
-                            Text("Cards left: ${info.cardsLeft}")
-                        }
-                    }
-                }
-            }
-        ) { padding ->
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
-                modifier = Modifier
-                    .padding(padding)
-                    .fillMaxWidth()
-            ) {
-                info.fieldSlots.forEach { fieldSlot ->
-                    Column(
-                        modifier = Modifier.weight(1f)
-                    ) {
-                        DropTarget<CardLocation>(
-                            onDrop = {
-                                it?.let { cardLocation ->
-                                    if (fieldCheck(cardLocation.card, fieldSlot.value)) {
-                                        when (cardLocation.location) {
-                                            -1 -> {
-                                                fieldSlot.value.addCard(cardLocation.card)
-                                                info.drawList.remove(cardLocation.card)
+                                } else {
+                                    LazyColumn(
+                                        verticalArrangement = Arrangement.spacedBy((-101.25).dp)
+                                    ) {
+                                        itemsIndexed(fieldSlot.value.list) { index, card ->
+                                            DragTarget(
+                                                dataToDrop = CardLocation(fieldSlot.key, card, index),
+                                                modifier = Modifier.padding(top = 4.dp)
+                                            ) {
+                                                PlayingCard(
+                                                    card = card,
+                                                    modifier = cardSizeModifier
+                                                )
                                             }
-
-                                            else -> {
-                                                info.fieldSlots[cardLocation.location]?.let { f: FieldSlot ->
-                                                    fieldSlot.value.addCards(f.removeCards(cardLocation.place))
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        ) { d, f ->
-                            if (fieldSlot.value.list.isEmpty()) {
-                                EmptyCard(
-                                    modifier = cardSizeModifier.fillMaxSize(),
-                                    border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
-                                )
-                            } else {
-                                LazyColumn(
-                                    verticalArrangement = Arrangement.spacedBy((-101.25).dp)
-                                ) {
-                                    itemsIndexed(fieldSlot.value.list) { index, card ->
-                                        DragTarget(
-                                            dataToDrop = CardLocation(fieldSlot.key, card, index),
-                                            modifier = Modifier.padding(top = 4.dp)
-                                        ) {
-                                            PlayingCard(
-                                                card = card,
-                                                modifier = cardSizeModifier
-                                            )
                                         }
                                     }
                                 }
