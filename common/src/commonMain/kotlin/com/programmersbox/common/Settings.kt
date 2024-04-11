7660c0261c41a41eadc08c3633eda833963d5723
jacobrein <jacob.rein@gabb.com>
2024-04-11 13:06:08 -0600

    Update common podspec and shell script paths
    This commit updates the common podspec and shell scripts to reflect new checksum details. It also introduces changes to common.podspec and modified the gradle path in the shell scripts.
    
    - Corrected the checksum details in podfile.lock
    - Added new functionalities in Settings.kt
    - Updated project build configurations in project.pbxproj
    - Revised gradle path in common.podspec and common.podspec.json

--- common/src/commonMain/kotlin/com/programmersbox/common/Settings.kt
+++ common/src/commonMain/kotlin/com/programmersbox/common/Settings.kt
@@ -178,6 +178,206 @@ enum class CardBack(
         )
     },
 
+    MagicCircle({ null }) {
+        @Composable
+        override fun toModifier(): Modifier = Modifier.shaderBackground(
+            remember {
+                object : ShaderDelegate() {
+                    override val sksl: String = """
+                        uniform float uTime;
+uniform vec3 uResolution;
+
+const float M_PI = 3.1415926535897932384626433832795;
+float M_PI05 = (M_PI * 0.5);
+
+vec2 rotate(vec2 v, float c, float s){
+	return vec2(v.x*c - v.y*s, v.x*s + v.y*c);
+}
+
+vec2 rotate(vec2 v, float r){
+	return rotate(v, cos(r), sin(r));
+}
+
+float boxLength(vec2 pos) {
+	vec2 q = abs(pos);
+	return max(q.x, q.y);
+}
+
+float capsuleLength(vec2 pos, vec2 dir) {
+	vec2 ba = -dir;
+	vec2 pa = pos + ba;
+	ba *= 2.0;
+	return length(pa - ba * clamp(dot(pa, ba) / dot(ba, ba), -1.0, 1.0));
+} 
+
+float triangleLength(vec2 p) {
+    p.y += 0.32;
+	return max(abs(p.x * 1.8) + p.y, 1.0 - p.y * 1.8) * 0.75;
+}
+
+vec2 fracOrigin(vec2 v){
+	return (fract(v) - 0.5) * 2.0;
+}
+
+float Sa(vec2 pos){
+ 	float a = capsuleLength(pos + vec2(0.0, -0.7), vec2(0.5, 0.0));   
+ 	float b = capsuleLength(pos + vec2(-0.3, -0.3), vec2(0.3, 1.3));  
+    float c = capsuleLength(pos + vec2(0.3, -0.5), vec2(0, 0.5)); 
+    return min(min(a, b), c);
+}
+
+float Ke(vec2 pos){
+ 	float a = capsuleLength(pos + vec2(0.0, -0.3), vec2(0.4, 0.0));   
+ 	float b = capsuleLength(pos + vec2(0.7, -0.3), vec2(0.5, 0.6));  
+    float c = capsuleLength(pos + vec2(0.1, 0.7), vec2(0.3, 0.3));  
+    return min(min(a, b), c);
+}
+
+float To(vec2 pos){
+ 	float a = capsuleLength(pos + vec2(0.6, 0.0), vec2(0.4, 1.0));   
+ 	float b = capsuleLength(pos + vec2(0.0, 0.0), vec2(1.0, -0.8) * 0.4);    
+    return min(a, b);
+}
+
+float Ba(vec2 pos){
+ 	float a = capsuleLength(pos + vec2(0.8, 0.0), vec2(0.2, 1.0));   
+ 	float b = capsuleLength(pos + vec2(-0.8, 0.0), vec2(-0.2, 1.0));     
+    float c = length(pos + vec2(-1.0, -1.3));
+    float d = length(pos + vec2(-1.2, -0.8));
+    return min(min(min(a, b), c), d);
+}
+
+float Saketoba(vec2 pos, float power){
+    return dot(vec4(1.0), power / (0.01 + vec4(Sa(pos), Ke(pos + vec2(-3.0, 0.0)), To(pos + vec2(-6.0, 0.0)), Ba(pos + vec2(-9.0, 0.0)))));
+}
+
+float smoothstepLine(float lower, float upper, float value, float width){
+    width *= 0.5;
+    return smoothstep(lower - width, lower, value) * (1.0 - smoothstep(upper, upper + width, value));
+}
+
+float smoothLine(float value, float target, float width){
+    return width / abs(value - target);
+}
+
+vec2 smoothLine2(float value, float target, float width){
+    return vec2(step(0.0, value - target), width / abs(value - target));
+}
+
+float circleTriangle(vec2 pos){
+    float circle = length(pos * 0.5);
+    float triangle = triangleLength(pos * 0.3);    
+    return smoothLine(circle, 1.0, 0.025) + smoothLine(triangle, 1.0, 0.025);
+}
+
+vec2 circleTriangle2(vec2 pos){
+    float circle2 = length(pos * 0.35);
+    vec2 ret = smoothLine2(circle2, 1.0, 0.025);
+    ret.y += circleTriangle(pos);
+    return ret;
+}
+
+float atan2(in float y, in float x)
+{
+    return x == 0.0 ? sign(y) * M_PI05 : atan(y, x);
+}
+
+vec2 polar(vec2 uv) {
+	float r = length(uv);
+	float s = atan2(uv.y, uv.x) / M_PI;
+	return vec2(r, s);
+}
+
+float SakeobaCircle(vec2 pos){
+    vec2 pp = polar(rotate(pos, -uTime) * 0.75);
+    vec2 md = mod(rotate(pp * vec2(2.0, 32.0), M_PI05), vec2(16.0, 4.0));
+    return Saketoba(md - vec2(3.5, 1.5), 0.05) * smoothstepLine(6.5, 7.5, pp.x, 1.5) * min(min(1.0, md.x), 16.0 - md.x);
+}
+
+float SakeobaCircle2(vec2 pos, float scale, float x, float y, float x2, float y2, float lower, float upper, float r){
+    vec2 pp = polar(rotate(pos, r) * scale);
+    return Saketoba(mod(rotate(pp * vec2(x, y), M_PI05), vec2(x2, y2)) - 1.5, 0.03) * smoothstepLine(lower, upper, pp.x, 0.2);
+}
+
+vec4 main( vec2 fragCoord )
+{
+    vec2 uv = (fragCoord.xy - uResolution.xy * 0.5) / uResolution.yy * 20.0;     
+      
+    uv *= clamp(uTime * 0.25, 0.0, 1.0) * 1.1;
+        
+    uv = rotate(uv, uTime * 0.3);
+    
+    vec2 uv2 = fragCoord.xy / uResolution.xy;
+    
+    float len = length(uv);
+    
+    vec2 c2 = circleTriangle2(uv * 1.4 + vec2(0.0, 8.0));
+    vec2 c3 = circleTriangle2(uv * 1.4 + rotate(vec2(0.0, 8.0), M_PI * 2.0 * 0.3333));
+    vec2 c4 = circleTriangle2(uv * 1.4 + rotate(vec2(0.0, 8.0), M_PI * 2.0 * 0.6666));
+   
+    float mask = clamp(c2.x * c3.x * c4.x, 0.0, 1.0);  
+        
+    float color1 = SakeobaCircle(uv) * 4.0
+  		
+        + (SakeobaCircle2(uv, 0.995, 8.0, 64.0, 12.0, 4.0, 7.5, 8.0, 5.0 + uTime * 0.2)
+        + smoothLine(len, 11.2, 0.1)
+        + smoothLine(len, 10.8, 0.1)
+        + smoothLine(len, 8.2, 0.01)
+        + smoothLine(len, 8.0, 0.02)
+        + smoothLine(len, 7.5, 0.01)
+        + smoothLine(len, 7.3, 0.01)
+        
+        + SakeobaCircle2(uv, 1.1, 8.0, 64.0, 12.0, 4.0, 7.5, 7.9, 5.0 + uTime * 0.8) * -1.0
+        + 0.0 * SakeobaCircle2(uv, 1.2, 8.0, 64.0, 12.0, 4.0, 7.5, 7.9, 15.0 + uTime * 0.1564)
+        + smoothLine(len, 6.7, 0.02)
+           
+        + SakeobaCircle2(uv, 1.45, 8.0, 64.0, 12.0, 4.0, 7.5, 7.9, 15.0 + uTime * 0.2418654) * -1.0
+        + smoothLine(len, 5.0, 0.02)
+        + smoothLine(len, 5.5, 0.02)
+        
+        + SakeobaCircle2(uv, 2.15, 8.0, 64.0, 12.0, 4.0, 7.5, 7.9, 35.0 + uTime * 0.34685) * -1.0
+        + SakeobaCircle2(uv, 2.25, 8.0, 64.0, 12.0, 4.0, 7.5, 7.9, 135.0 + uTime * 0.114)
+        + SakeobaCircle2(uv, 1.8, 8.0, 64.0, 12.0, 4.0, 7.5, 7.9, 532.0 + uTime * 0.54158)
+        + 0.015 / abs(boxLength(rotate(uv, M_PI05 * 0.0 - uTime * 0.5)) - 4.5)
+        + 0.005 / abs(boxLength(rotate(uv, M_PI05 * 0.25 - uTime * 0.5)) - 4.5) * -1.0
+        + 0.015 / abs(boxLength(rotate(uv, M_PI05 * 0.5 - uTime * 0.5)) - 4.5)
+        + 0.005 / abs(boxLength(rotate(uv, M_PI05 * 0.75 - uTime * 0.5)) - 4.5) * -1.0
+           
+          ) * mask
+      
+        + circleTriangle(uv)
+        ;
+    
+    float core = pow(0.5, 3.0);
+    float core2 = 0.3;
+  
+    float color2 =   
+        + core2 * c2.y
+    	+ core2 * c3.y
+     	+ core2 * c4.y
+        + core / abs(boxLength(uv * vec2(8.0, 0.5) - vec2(0.0, 2.9)) - 1.0)
+        + core / abs(boxLength(rotate(uv, M_PI * 2.0 * 0.3333) * vec2(8.0, 0.5) - vec2(0.0, 2.9)) - 1.0)
+        + core / abs(boxLength(rotate(uv, M_PI * 2.0 * 0.6666) * vec2(8.0, 0.5) - vec2(0.0, 2.9)) - 1.0)
+	;
+    
+    vec3 col = vec3(
+        (color1 + color2 * (1.0)) * 1.5,
+    	color1 + color2,
+        color1 * -0.2 //change this for purple
+    ) * (mask + 1.0);
+  
+    col += (floor(4.0) - 20.0) * smoothstep(8.0, 8.1, len) * 0.5;
+    
+    //col *= clamp(2.0, 0.0, 1.0);
+    
+    return vec4(col, 1.0);
+}
+        """.trimIndent()
+                }
+            }
+        )
+    },
+
     BigBang({ null }) {
         @Composable
         override fun toModifier(): Modifier = Modifier.shaderBackground(
